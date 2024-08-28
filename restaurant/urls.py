1602b7432e20070cd99dda313f5bd78457177de7
Autumn Wanicka <autumn.wanicka@kingsmensoftware.com>
2024-08-28 10:30:46 -0400

    create api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu-items/', views.MenuItemView.as_view(), name='menu-items'),
+    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
+    
 ]
